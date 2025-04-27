2a49467ea0864b768d2f84ea4771bbf73b1e98ba
Amasha <amashasamarakoon4@gmail.com>
2025-04-27 20:54:04 +0530

    index change

--- admin/index.php
+++ admin/index.php
@@ -50,9 +50,9 @@ require ('inc/db_config.php');
    
    if($res->num_rows==1){
      $row =mysqli_fetch_assoc($res);
-     session_star();
+     session_start();
      $_SESSION['adminLogin']=true;
-     $_SESSION['adminId']= $row[sr_no];
+     $_SESSION['adminId']= $row['sr_no'];
      redirect('dashboard.php');
      
    
