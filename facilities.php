a93b775d5c1f464d52c70e98b36165c7535b25f7
pubudugunawardhana <iit22010@std.uwu.ac.lk>
2025-07-12 10:48:07 +0530

    update facilities page

--- facilities.php
+++ facilities.php
@@ -1,134 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mirissa 360 - FACILITIES</title>
-
-    <?php require('inc/links.php'); ?>
-    <link rel="stylesheet" href="css/common.css" />
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-   
-    <style>
-       .pop:hover{
-            border-top-color:var(--teal) !important;
-            transform: scale(1.03);
-            transition: all 0.3s;
-       }
-    </style>
-
-
-
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php require('inc/links.php'); ?>
+  <title><?php echo $settings_r['site_title'] ?> - FACILITIES</title>
+  <style>
+    .pop:hover{
+      border-top-color: var(--teal) !important;
+      transform: scale(1.03);
+      transition: all 0.3s;
+    }
+  </style>
 </head>
-
 <body class="bg-light">
 
-    <!-- header Link -->
+  <?php require('inc/header.php'); ?>
 
-    <?php require('inc/header.php'); ?>
+  <div class="my-5 px-4">
+    <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
+    <div class="h-line bg-dark"></div>
+    <p class="text-center mt-3">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Temporibus incidunt odio quos <br> dolore commodi repudiandae 
+      tenetur consequuntur et similique asperiores.
+    </p>
+  </div>
 
-    <!-- our facilities -->
+  <div class="container">
+    <div class="row">
+      <?php 
+        $res = selectAll('facilities');
+        $path = FACILITIES_IMG_PATH;
 
-    <div class="my-5 px-4">
-        <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
-        <div class="h-line bg-dark"></div>
-        <p class="text-center mt-3">
-            At Mirissa 360, we are committed to providing an unforgettable stay with world-class amenities and thoughtful services.
-            Whether you’re traveling for business or leisure, our facilities are designed to offer comfort, convenience, and relaxation at every moment.
-            Enjoy modern rooms, fine dining options, relaxing spa treatments, a fully equipped fitness center, and seamless business services.
-            Experience the perfect blend of luxury and hospitality — all under one roof.
-        </p>
-        <br>
-        <br>
-        <hr>
-
+        while($row = mysqli_fetch_assoc($res)){
+          echo<<<data
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+              <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+                <div class="d-flex align-items-center mb-2">
+                  <img src="$path$row[icon]" width="40px">
+                  <h5 class="m-0 ms-3">$row[name]</h5>
+                </div>
+                <p>$row[description]</p>
+              </div>
+            </div>
+          data;
+        }
+      ?>
     </div>
+  </div>
 
-    <!-- facilities section -->
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                        <img src="Images/facilities/IMG_43553.svg" width="40px">
-                        <h5 class="m-0 ms-3">Wifi</h5>
-                    </div>
 
-                    
-                    <p>Stay connected with fast, free Wi-Fi available throughout the hotel — in your room, lobby, and even by the pool and spa.</p>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                        <img src="Images/facilities/IMG_49949.svg" width="40px">
-                        <h5 class="m-0 ms-3">Air Conditioning</h5>
-                    </div>
-
-                    
-                    <p>Enjoy a cool and comfortable stay with individually controlled air conditioning in all guest rooms and common areas.</p>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                        <img src="Images/facilities/IMG_41622.svg" width="40px">
-                        <h5 class="m-0 ms-3">Telivision</h5>
-                    </div>
-
-                    
-                    <p>Relax with a wide selection of local and international channels on our flat-screen TVs, available in every room.</p>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                        <img src="Images/facilities/IMG_96423.svg" width="40px">
-                        <h5 class="m-0 ms-3">Room Heater</h5>
-                    </div>
-
-                    
-                    <p>Stay warm and cozy with our in-room heating system, ensuring perfect comfort during cooler days.</p>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                        <img src="Images/facilities/IMG_27079.svg" width="40px">
-                        <h5 class="m-0 ms-3">Fire Alarm System</h5>
-                    </div>
-
-                    
-                    <p>Your safety is our priority. Our advanced fire alarm system is installed throughout the hotel to ensure prompt alerts and peace of mind.</p>
-                </div>
-            </div>
-
-            <div class="col-lg-4 col-md-6 mb-5 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
-                    <div class="d-flex align-items-center mb-2">
-                        <img src="Images/facilities/IMG_47816.svg" width="40px">
-                        <h5 class="m-0 ms-3">Spa</h5>
-                    </div>
-
-                    
-                    <p>Indulge in ultimate relaxation with our luxurious spa services, offering rejuvenating treatments, massages, and wellness therapies.</p>
-                </div>
-            </div>
-
-        </div>
-
-    </div>
-
-    <?php require('inc/footer.php'); ?>
-
-    <br><br>
+  <?php require('inc/footer.php'); ?>
 
 </body>
-
 </html>
