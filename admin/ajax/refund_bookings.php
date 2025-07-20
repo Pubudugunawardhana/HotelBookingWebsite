7446602c7f9c0a4741d3e5b17502789aa3197f9a
pubudugunawardhana <iit22010@std.uwu.ac.lk>
2025-07-20 21:22:44 +0530

    fix bugs

--- admin/ajax/refund_bookings.php
+++ admin/ajax/refund_bookings.php
@@ -51,7 +51,7 @@
             <b>Date:</b> $date
           </td>
           <td>
-            <b>₹$data[trans_amt]</b> 
+            <b>LKR$data[trans_amt]</b> 
           </td>
           <td>
             <button type='button' onclick='refund_booking($data[booking_id])' class='btn btn-success btn-sm fw-bold shadow-none'>
